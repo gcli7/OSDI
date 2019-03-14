198f6427ef8c57335ad53174d1e01d7d4c2d2cef
gcli <star106121@gmail.com>
2019-03-14 13:58:30 +0800

    Lab2 demo. Load hello program at 0x0100.

--- linux-0.11/boot/bootsect.s
+++ linux-0.11/boot/bootsect.s
@@ -5,7 +5,7 @@
 # 0x3000 is 0x30000 bytes = 196kB, more than enough for current
 # versions of linux
 #
-	.equ SYSSIZE, 0x3000
+	.equ    SYSSIZE, 0x3000
 #
 #	bootsect.s		(C) 1991 Linus Torvalds
 #
@@ -33,134 +33,118 @@
 	begbss:
 	.text
 
-	.equ SETUPLEN, 4		# nr of setup-sectors
-	.equ BOOTSEG, 0x07c0		# original address of boot-sector
-	.equ INITSEG, 0x9000		# we move boot here - out of the way
-	.equ SETUPSEG, 0x9020		# setup starts here
-	.equ SYSSEG, 0x1000		# system loaded at 0x10000 (65536).
-	.equ ENDSEG, SYSSEG + SYSSIZE	# where to stop loading
+	.equ    SETUPLEN, 4		# nr of setup-sectors
+	.equ    BOOTSEG, 0x07c0		# original address of boot-sector
+	.equ    INITSEG, 0x9000		# we move boot here - out of the way
+	.equ    SETUPSEG, 0x9020		# setup starts here
+	.equ    SYSSEG, 0x1000		# system loaded at 0x10000 (65536).
+	.equ    ENDSEG, SYSSEG + SYSSIZE	# where to stop loading
 
 # ROOT_DEV:	0x000 - same type of floppy as boot.
 #		0x301 - first partition on first drive etc
-	.equ ROOT_DEV, 0x301
+	.equ    ROOT_DEV, 0x301
 	ljmp    $BOOTSEG, $_start
 _start:
-	mov	$BOOTSEG, %ax
-	mov	%ax, %ds
-	mov	$INITSEG, %ax
-	mov	%ax, %es
-	mov	$256, %cx
-	sub	%si, %si
-	sub	%di, %di
+	mov 	$BOOTSEG, %ax
+	mov 	%ax, %ds
+	mov 	$INITSEG, %ax
+	mov	    %ax, %es
+	mov	    $256, %cx
+	sub 	%si, %si
+	sub 	%di, %di
 	rep	
 	movsw
 	ljmp	$INITSEG, $go
-go:	mov	%cs, %ax
-	mov	%ax, %ds
-	mov	%ax, %es
+go:	mov 	%cs, %ax
+	mov 	%ax, %ds
+	mov 	%ax, %es
 # put stack at 0x9ff00.
-	mov	%ax, %ss
-	mov	$0xFF00, %sp		# arbitrary value >>512
+	mov 	%ax, %ss
+	mov 	$0xFF00, %sp		# arbitrary value >>512
 
 print_start:
-        mov     $0x0E, %ah
-        mov     $_start, %al
-        int     $0x10
-        mov     $5, %al
-        int     $0x10
+    mov     $0x0E, %ah
+    mov     $_start, %al
+    int     $0x10
+    mov     $5, %al
+    int     $0x10
 
 get_input:
 	mov     $0x03, %ah              # read cursor pos
-        xor     %bh, %bh
-        int     $0x10
-        mov     $54, %cx
-        mov     $0x0007, %bx            # page 0, attribute 7 (normal)
-        #lea    msg_retype, %bp
-        mov     $msg_retype, %bp
-        mov     $0x1301, %ax            # write string, move cursor
-        int     $0x10
+    xor     %bh, %bh
+    int     $0x10
+    mov     $54, %cx
+    mov     $0x0007, %bx            # page 0, attribute 7 (normal)
+    #lea    msg_retype, %bp
+    mov     $msg_retype, %bp
+    mov     $0x1301, %ax            # write string, move cursor
+    int     $0x10
 
-	mov	$0x00, %ah		# get input character
-	int	$0x16
-	cmp	$49, %al
-	je	load_setup
-	cmp	$50, %al
-	je	load_hello
-	jmp	get_input
+	mov 	$0x00, %ah		# get input character
+	int	    $0x16
+	cmp	    $49, %al
+	je  	load_setup
+	cmp 	$50, %al
+	je  	load_hello
+	jmp 	get_input
 
 load_hello:
 	mov     $0x0000, %dx            # drive 0, head 0
-        mov     $0x0002, %cx            # sector 2, track 0
-        mov     $0x0200, %bx            # address = 512, in INITSEG
-        mov     $0x0202, %ax                # service 2, nr of sectors
-        int     $0x13                   # read it
-        jnc     ok_load_hello           # ok - continue
-        mov     $0x0000, %dx
-        mov     $0x0000, %ax            # reset the diskette
-        int     $0x13
-        jmp     load_hello
-
-ok_load_hello:
-        mov     $0x00, %dl
-        mov     $0x0800, %ax            # AH=8 is get drive parameters
-        int     $0x13
-        mov     $0x00, %ch
-        #seg cs
-        mov     %cx, %cs:sectors+0      # %cs means sectors is in %cs
-        mov     $INITSEG, %ax
-        mov     %ax, %es
-	jmp	print_meg
-
-	#.equ sel_cs0, 0x0100		#select for code segment 0
-	#ljmp $sel_cs0, $0		#Jump to hello
+    mov     $0x0002, %cx            # sector 2, track 0
+    mov     $0x0100, %ax
+    mov     %ax, %es
+    mov     $0x0000, %bx            # address = 512, in INITSEG
+    mov     $0x0201, %ax            # service 2, nr of sectors
+    int     $0x13                   # read it
+    ljmp    $0x0100, $0
 
 # load the setup-sectors directly after the bootblock.
 # Note that 'es' is already set up.
 
 load_setup:
-	mov	$0x0000, %dx		# drive 0, head 0
-	mov	$0x0003, %cx		# sector 2, track 0
-	mov	$0x0200, %bx		# address = 512, in INITSEG
+	mov	    $0x0000, %dx		# drive 0, head 0
+	mov	    $0x0003, %cx		# sector 2, track 0
+	mov 	$0x0200, %bx		# address = 512, in INITSEG
 	.equ    AX, 0x0200+SETUPLEN
 	mov     $AX, %ax		# service 2, nr of sectors
-	int	$0x13			# read it
-	jnc	ok_load_setup		# ok - continue
-	mov	$0x0000, %dx
-	mov	$0x0000, %ax		# reset the diskette
-	int	$0x13
-	jmp	load_setup
+	int 	$0x13			# read it
+	jnc 	ok_load_setup		# ok - continue
+	mov 	$0x0000, %dx
+	mov 	$0x0000, %ax		# reset the diskette
+	int 	$0x13
+	jmp 	load_setup
 
 ok_load_setup:
 
 # Get disk drive parameters, specifically nr of sectors/track
 
-	mov	$0x00, %dl
-	mov	$0x0800, %ax		# AH=8 is get drive parameters
-	int	$0x13
-	mov	$0x00, %ch
+	mov 	$0x00, %dl
+	mov 	$0x0800, %ax		# AH=8 is get drive parameters
+	int 	$0x13
+	mov 	$0x00, %ch
 	#seg cs
-	mov	%cx, %cs:sectors+0	# %cs means sectors is in %cs
-	mov	$INITSEG, %ax
-	mov	%ax, %es
+	mov 	%cx, %cs:sectors+0	# %cs means sectors is in %cs
+	mov 	$INITSEG, %ax
+	mov 	%ax, %es
 
 # Print some inane message
 print_meg:
-	mov	$0x03, %ah		# read cursor pos
-	xor	%bh, %bh
-	int	$0x10
+	mov 	$0x03, %ah		# read cursor pos
+	xor 	%bh, %bh
+	int 	$0x10
 	
-	mov	$26, %cx
-	mov	$0x0007, %bx		# page 0, attribute 7 (normal)
-	#lea	msg1, %bp
+	mov 	$26, %cx
+	mov 	$0x0007, %bx		# page 0, attribute 7 (normal)
+	#lea 	msg1, %bp
 	mov     $msg1, %bp
-	mov	$0x1301, %ax		# write string, move cursor
-	int	$0x10
+	mov 	$0x1301, %ax		# write string, move cursor
+	int 	$0x10
 
 # ok, we've written the message, now
 # we want to load the system (at 0x10000)
 
-	mov	$SYSSEG, %ax
-	mov	%ax, %es		# segment of 0x010000
+	mov 	$SYSSEG, %ax
+	mov 	%ax, %es		# segment of 0x010000
 	call	read_it
 	call	kill_motor
 
@@ -170,22 +154,22 @@ print_meg:
 # on the number of sectors that the BIOS reports currently.
 
 	#seg cs
-	mov	%cs:root_dev+0, %ax
-	cmp	$0, %ax
-	jne	root_defined
+	mov 	%cs:root_dev+0, %ax
+	cmp 	$0, %ax
+	jne 	root_defined
 	#seg cs
-	mov	%cs:sectors+0, %bx
-	mov	$0x0208, %ax		# /dev/ps0 - 1.2Mb
-	cmp	$15, %bx
-	je	root_defined
-	mov	$0x021c, %ax		# /dev/PS0 - 1.44Mb
-	cmp	$18, %bx
-	je	root_defined
+	mov 	%cs:sectors+0, %bx
+	mov 	$0x0208, %ax		# /dev/ps0 - 1.2Mb
+	cmp 	$15, %bx
+	je  	root_defined
+	mov 	$0x021c, %ax		# /dev/PS0 - 1.44Mb
+	cmp 	$18, %bx
+	je  	root_defined
 undef_root:
-	jmp undef_root
+	jmp     undef_root
 root_defined:
 	#seg cs
-	mov	%ax, %cs:root_dev+0
+	mov	    %ax, %cs:root_dev+0
 
 # after that (everyting loaded), we jump to
 # the setup-routine loaded directly after
@@ -204,24 +188,25 @@ head:	.word 0			# current head
 track:	.word 0			# current track
 
 read_it:
-	mov	%es, %ax
+	mov	    %es, %ax
 	test	$0x0fff, %ax
-die:	jne 	die			# es must be at 64kB boundary
+die:
+    jne 	die			# es must be at 64kB boundary
 	xor 	%bx, %bx		# bx is starting address within segment
 rp_read:
 	mov 	%es, %ax
  	cmp 	$ENDSEG, %ax		# have we loaded all yet?
-	jb	ok1_read
+	jb  	ok1_read
 	ret
 ok1_read:
 	#seg cs
-	mov	%cs:sectors+0, %ax
-	sub	sread, %ax
-	mov	%ax, %cx
-	shl	$9, %cx
-	add	%bx, %cx
+	mov	    %cs:sectors+0, %ax
+	sub 	sread, %ax
+	mov 	%ax, %cx
+	shl 	$9, %cx
+	add 	%bx, %cx
 	jnc 	ok2_read
-	je 	ok2_read
+	je 	    ok2_read
 	xor 	%ax, %ax
 	sub 	%bx, %ax
 	shr 	$9, %ax
@@ -237,48 +222,49 @@ ok2_read:
 	jne 	ok4_read
 	incw    track 
 ok4_read:
-	mov	%ax, head
-	xor	%ax, %ax
+	mov 	%ax, head
+	xor 	%ax, %ax
 ok3_read:
-	mov	%ax, sread
-	shl	$9, %cx
-	add	%cx, %bx
-	jnc	rp_read
-	mov	%es, %ax
-	add	$0x1000, %ax
-	mov	%ax, %es
-	xor	%bx, %bx
-	jmp	rp_read
+	mov 	%ax, sread
+	shl 	$9, %cx
+	add 	%cx, %bx
+	jnc 	rp_read
+	mov 	%es, %ax
+	add 	$0x1000, %ax
+	mov 	%ax, %es
+	xor 	%bx, %bx
+	jmp 	rp_read
 
 read_track:
 	push	%ax
 	push	%bx
 	push	%cx
 	push	%dx
-	mov	track, %dx
-	mov	sread, %cx
-	inc	%cx
-	mov	%dl, %ch
-	mov	head, %dx
-	mov	%dl, %dh
-	mov	$0, %dl
-	and	$0x0100, %dx
-	mov	$2, %ah
-	int	$0x13
-	jc	bad_rt
-	pop	%dx
-	pop	%cx
-	pop	%bx
-	pop	%ax
+	mov 	track, %dx
+	mov 	sread, %cx
+	inc 	%cx
+	mov 	%dl, %ch
+	mov 	head, %dx
+	mov 	%dl, %dh
+	mov 	$0, %dl
+	and 	$0x0100, %dx
+	mov 	$2, %ah
+	int 	$0x13
+	jc  	bad_rt
+	pop 	%dx
+	pop 	%cx
+	pop 	%bx
+	pop 	%ax
 	ret
-bad_rt:	mov	$0, %ax
-	mov	$0, %dx
-	int	$0x13
-	pop	%dx
-	pop	%cx
-	pop	%bx
-	pop	%ax
-	jmp	read_track
+bad_rt:
+    mov 	$0, %ax
+	mov 	$0, %dx
+	int 	$0x13
+	pop 	%dx
+	pop 	%cx
+	pop 	%bx
+	pop	    %ax
+	jmp 	read_track
 
 #/*
 # * This procedure turns off the floppy drive motor, so
@@ -286,11 +272,11 @@ bad_rt:	mov	$0, %ax
 # * don't have to worry about it later.
 # */
 kill_motor:
-	push	%dx
-	mov	$0x3f2, %dx
-	mov	$0, %al
+	push 	%dx
+	mov 	$0x3f2, %dx
+	mov 	$0, %al
 	outsb
-	pop	%dx
+	pop 	%dx
 	ret
 
 sectors:
