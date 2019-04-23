77464978249370e58f0b7ab641006ca22a27f09f
gcli <star106121@gmail.com>
2019-04-23 22:20:11 +0800

    Complete kernel/task.h.

--- kernel/task.h
+++ kernel/task.h
@@ -3,30 +3,30 @@
 
 #include <inc/trap.h>
 #include <kernel/mem.h>
-#define NR_TASKS	10
-#define TIME_QUANT	100
+#define NR_TASKS    10
+#define TIME_QUANT  100
 
 typedef enum
 {
-	TASK_FREE = 0,
-	TASK_RUNNABLE,
-	TASK_RUNNING,
-	TASK_SLEEP,
-	TASK_STOP,
+    TASK_FREE = 0,
+    TASK_RUNNABLE,
+    TASK_RUNNING,
+    TASK_SLEEP,
+    TASK_STOP,
 } TaskState;
 
 // Each task's user space
-#define USR_STACK_SIZE	(40960)
+#define USR_STACK_SIZE  (40960)
 
 typedef struct
 {
-	int task_id;
-	int parent_id;
-	struct Trapframe tf; //Saved registers
-	int32_t remind_ticks;
-	TaskState state;	//Task state
-  pde_t *pgdir;  //Per process Page Directory
-	
+    int task_id;
+    int parent_id;
+    struct Trapframe tf; //Saved registers
+    int32_t remind_ticks;
+    TaskState state;    //Task state
+    pde_t *pgdir;  //Per process Page Directory
+    
 } Task;
 
 void task_init();
@@ -36,5 +36,7 @@ void env_pop_tf(struct Trapframe *tf);
  * Interface for real implementation of kill and fork
  * Since their real implementation should be in kernel/task.c
  */
+int sys_fork();
+void sys_kill(int);
 
 #endif
