163174f49def223e740685a73dad8476e458a8ec
gcli <star106121@gmail.com>
2019-05-21 22:32:11 +0800

    Complete the fat_*() functions.

--- kernel/fs/fs_ops.c
+++ kernel/fs/fs_ops.c
@@ -47,13 +47,13 @@ extern struct fs_dev fat_fs;
 */
 int fat_mount(struct fs_dev *fs, const void* data)
 {
-
+    return -f_mount(fs->data, fs->path, 1);
 }
 
 /* Note: Just call f_mkfs at root path '/' */
 int fat_mkfs(const char* device_name)
 {
-
+    return -f_mkfs("/", 0, 0);
 }
 
 /* Note: Convert the POSIX's open flag to elmfat's flag.
@@ -62,24 +62,59 @@ int fat_mkfs(const char* device_name)
 */
 int fat_open(struct fs_fd* file)
 {
+    int flag = 0;
+    if(file->flags == O_RDONLY)
+        flag |= FA_READ;
+    if(file->flags & O_WRONLY)
+        flag |= FA_WRITE;
+    if(file->flags & O_RDWR)
+        flag |= FA_READ | FA_WRITE;
+    if(file->flags & O_ACCMODE)
+        flag &= 0x3;
+    if((file->flags & O_CREAT) && !(file->flags & O_TRUNC))
+        flag |= FA_CREATE_NEW;
+    if(file->flags & O_TRUNC)
+        flag |= FA_CREATE_ALWAYS;
+
+    int retval = f_open(file->data, file->path, flag);
+    if(file->flags & O_APPEND)
+        fat_lseek(file, ((FIL*)file->data)->obj.objsize);
+    return -retval;
 }
 
 int fat_close(struct fs_fd* file)
 {
-
+    return -f_close(file->data);
 }
 int fat_read(struct fs_fd* file, void* buf, size_t count)
 {
+    unsigned int len;
+    int retval = f_read(file->data, buf, count, &len);
+    if (retval)
+        return -retval;
 
+    file->pos += len;
+    return len;
 }
 int fat_write(struct fs_fd* file, const void* buf, size_t count)
 {
+    unsigned int len;
+    int retval = f_write(file->data, buf, count, &len);
+    if (retval)
+        return -retval;
+
+    file->pos += len;
+    if (file->pos > file->size)
+        file->size = file->pos;
+    return len;
 }
 int fat_lseek(struct fs_fd* file, off_t offset)
 {
+    return -f_lseek(file->data, offset);
 }
 int fat_unlink(struct fs_fd* file, const char *pathname)
 {
+    return -f_unlink(pathname);
 }
 
 struct fs_ops elmfat_ops = {
@@ -93,6 +128,3 @@ struct fs_ops elmfat_ops = {
     .lseek = fat_lseek,
     .unlink = fat_unlink
 };
-
-
-
